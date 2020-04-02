7d5b997f7ed49ed197c6eaf9813c16dc8855e6b7
RIssa <rissa_cford@yahoo.com>
2020-04-01 17:26:07 -0700

    mostly working, but still can't generate a proper pdf

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const inquirer = require("inquirer");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
 const convertFactory = require("electron-html-to");
+const electron = require("electron");
+const axios = require("axios");
+const generateHTML = require("./generateHTML");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 
 function promptUser() {
@@ -26,185 +31,38 @@ function promptUser() {
     ]);
 }
 
-function generateHTML(answers) {
-    return
-    `
-        <!DOCTYPE html>
-      <html lang="en">
-         <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-            <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-            <title>Document</title>
-            <style>
-                @page {
-                  margin: 0;
-                }
-               *,
-               *::after,
-               *::before {
-               box-sizing: border-box;
-               }
-               html, body {
-               padding: 0;
-               margin: 0;
-               }
-               html, body, .wrapper {
-               height: 100%;
-               }
-               .wrapper {
-               background-color: ${colors[data.color].wrapperBackground};
-               padding-top: 100px;
-               }
-               body {
-               background-color: white;
-               -webkit-print-color-adjust: exact !important;
-               font-family: 'Cabin', sans-serif;
-               }
-               main {
-               background-color: #E9EDEE;
-               height: auto;
-               padding-top: 30px;
-               }
-               h1, h2, h3, h4, h5, h6 {
-               font-family: 'BioRhyme', serif;
-               margin: 0;
-               }
-               h1 {
-               font-size: 3em;
-               }
-               h2 {
-               font-size: 2.5em;
-               }
-               h3 {
-               font-size: 2em;
-               }
-               h4 {
-               font-size: 1.5em;
-               }
-               h5 {
-               font-size: 1.3em;
-               }
-               h6 {
-               font-size: 1.2em;
-               }
-               .photo-header {
-               position: relative;
-               margin: 0 auto;
-               margin-bottom: -50px;
-               display: flex;
-               justify-content: center;
-               flex-wrap: wrap;
-               background-color: ${colors[data.color].headerBackground};
-               color: ${colors[data.color].headerColor};
-               padding: 10px;
-               width: 95%;
-               border-radius: 6px;
-               }
-               .photo-header img {
-               width: 250px;
-               height: 250px;
-               border-radius: 50%;
-               object-fit: cover;
-               margin-top: -75px;
-               border: 6px solid ${colors[data.color].photoBorderColor};
-               box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-               }
-               .photo-header h1, .photo-header h2 {
-               width: 100%;
-               text-align: center;
-               }
-               .photo-header h1 {
-               margin-top: 10px;
-               }
-               .links-nav {
-               width: 100%;
-               text-align: center;
-               padding: 20px 0;
-               font-size: 1.1em;
-               }
-               .nav-link {
-               display: inline-block;
-               margin: 5px 10px;
-               }
-               .workExp-date {
-               font-style: italic;
-               font-size: .7em;
-               text-align: right;
-               margin-top: 10px;
-               }
-               .container {
-               padding: 50px;
-               padding-left: 100px;
-               padding-right: 100px;
-               }
-      
-               .row {
-                 display: flex;
-                 flex-wrap: wrap;
-                 justify-content: space-between;
-                 margin-top: 20px;
-                 margin-bottom: 20px;
-               }
-      
-               .card {
-                 padding: 20px;
-                 border-radius: 6px;
-                 background-color: ${colors[data.color].headerBackground};
-                 color: ${colors[data.color].headerColor};
-                 margin: 20px;
-               }
-               
-               .col {
-               flex: 1;
-               text-align: center;
-               }
-      
-               a, a:hover {
-               text-decoration: none;
-               color: inherit;
-               font-weight: bold;
-               }
-      
-               @media print { 
-                body { 
-                  zoom: .75; 
-                } 
-               }
-            </style>
-            </head>
-      
-            <body>
-               test html file
-            </body>
-            </html>`
-              }
+function writeFile(fileName, data) {
+  writeFileAsync(fileName, data)
+}
 
-              promptUser().then(function(answers) {
-                const html = generateHTML(answers);
-                return writeFileAsync("index.html". html);
-            
-            }).then(function() {
-                console.log("here is your profile!");
-            
-            }).catch(function(err) {
-                console.log(err);
-            });
+promptUser().then((data) => {
+  const queryUrl = `https://api.github.com/users/${data.github}`;
+  const starsUrl = `https://api.github.com/users/${data.github}/starred`;
+    axios.get(queryUrl).then((res) => {
+      axios.get(starsUrl).then((stars) => {
+        const html = generateHTML(data, res, stars);
+        writeFileAsync("github-portfolio.html", html);
+        readFileAsync("github-portfolio.html", "utf8").then(html);
+        
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF,
+          allowLocalFilesAccess: true
+        });
 
-  const conversion = convertFactory({
-    converterPath: convertFactory.converters.PDF
-  });
+      conversion({ html: html }, function(err, result) {
+        if(err) {
+          return console.error(err);
+        }
 
-  conversion({html:htmlString}, function(err, result) {
-    if (err) {
-      return console.error(err);
-    }
-  console.log(result.numberOfPages);
-  console.log(result.logs);
-  result.stream.pipe(fs.createWriteStream('/path/to/anywhere.pdf'));
-  conversion.kill();
-});
-            
+        console.log(result.numberOfPages);
+        console.log(result.logs);
+        result.stream.pipe(fs.createWriteStream('github-profile.pdf'));
+        conversion.kill();
+    
+      })
+      })
+    })
+}).catch(function(err) {
+  console.log(err)
+})
       
